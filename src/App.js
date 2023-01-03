c38f28e0e688b5a53737547137f019b169389150
Gustavo Takahashi <gustavokenjitakahashi@gmail.com>
2023-01-03 12:26:34 -0300

    grid layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import About from './components/About'
+import Contacts from './components/Contacts'
 import SocialLinks from './components/SocialLinks'
 import Name from './components/Name'
 import ProfessionalExperience from './components/ProfessionalExperience'
@@ -16,6 +17,7 @@ class App extends Component {
     return (
       <div className="App">
         <About />
+        <Contacts />
         <SocialLinks />
         <Name />
         <ProfessionalExperience />
